9180d135243aa7bf2746eed1c4426182072fc885
nelsonic <contact.nelsonic+github@gmail.com>
2016-06-14 16:22:20 +0100

    adds .travis.yml file to attempt to run the tests on Travis CI! see: https://github.com/nelsonic/learn-nightwatch/issues/8

--- bin/selenium_child_process.js
+++ bin/selenium_child_process.js
@@ -1,20 +1,27 @@
-console.log('Starting Selenium ...');
-require('daemon')();
+const selenium = require('selenium-download');
 const exec = require('child_process').exec;
+
 selenium.ensure('./bin', function(error) {
-   if (error) {
-      return callback(error);
-   }
-   exec('java -jar ./bin/selenium.jar', (error, stdout, stderr) => {
-     if (error) {
-       console.error(`exec error: ${error}`);
-       return;
-     }
-     if (stdout) {
-       console.log(`> ${stdout}`);
-     }
-     if (stderr) {
-       console.log(`>> ${stderr}`); // handle errors in your preferred way.
-     }
-   });
+  if (error) {
+    throw new Error(error);
+  } else {
+    return start();
+  }
 });
+
+function start () {
+  console.log('Starting Selenium ...');
+  require('daemon')();
+  exec('java -jar ./bin/selenium.jar', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    if (stdout) {
+      console.log(`> ${stdout}`);
+    }
+    if (stderr) {
+      console.log(`>> ${stderr}`); // handle errors in your preferred way.
+    }
+  });
+}
