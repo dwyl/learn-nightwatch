e710c98b572d0f9a6777d06e5d6691991dc72c90
nelsonic <contact.nelsonic+github@gmail.com>
2016-06-15 12:46:52 +0100

    [Travis-CI] still trying to debug on Travis ... #8 :cry:

--- bin/selenium_child_process.js
+++ bin/selenium_child_process.js
@@ -1,8 +1,6 @@
 const selenium = require('selenium-download');
 const execSync = require('child_process').execSync;
 
-console.log(' - - - - - - - - - - - - - - - - - -> process.env:', process.env);
-
 selenium.ensure('./bin', function(error) {
   if (error) {
     throw new Error(error);
@@ -12,14 +10,9 @@ selenium.ensure('./bin', function(error) {
 });
 
 function start () {
-  require('daemon')();
-  // var cmd = ;
-  execSync('nohup java -jar ./bin/selenium.jar &');
   console.log('Starting Selenium ...');
-  var seconds = 5;
-  while (new Date().getTime() <= (new Date().getTime() + (seconds * 1000))) {
-  ; // do nothing but wait...
-  }
-  console.log('done');
-  return;
+  require('daemon')();
+  execSync('java -jar ./bin/selenium.jar');
 }
+
+console.log(' - - - - - - - - - - - - - - - - - -> process.env:', process.env);
