e826773eda291e0fe3fabfb7d30fd9bde488fe46
nelsonic <contact.nelsonic+github@gmail.com>
2016-06-15 18:12:28 +0100

    make selenium status request inside sbin/selenium-download and dont attempt to start multiple instances! #8

--- bin/selenium_child_process.js
+++ bin/selenium_child_process.js
@@ -1,5 +1,6 @@
 const selenium = require('selenium-download');
 const execSync = require('child_process').execSync;
+const get = require('http').get;
 
 selenium.ensure('./bin', function(error) {
   if (error) {
@@ -10,9 +11,19 @@ selenium.ensure('./bin', function(error) {
 });
 
 function start () {
-  console.log('Starting Selenium ...');
-  require('daemon')();
-  execSync('java -jar ./bin/selenium.jar && sleep 2');
+  get('http://localhost:4444/wd/hub/status', function (res) {
+    if (res && res.statusCode === 200){
+      console.log('Selenium is running:');
+      res.resume();
+      res.on("data", function(chunk) {
+        console.log(JSON.stringify(JSON.parse(chunk), null, 2));
+      });
+      return;
+    }
+  }).on('error', (e) => {
+    console.log(`Got error: ${e.message}`);
+    console.log('Starting Selenium ...');
+    require('daemon')();
+    execSync('nohup java -jar ./bin/selenium.jar &');
+  });
 }
-
-// console.log(' - - - - - - - - - - - - - - - - - -> process.env:', process.env);
