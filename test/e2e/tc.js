74b87f56a05254172daab9b11a1b7cec69475fc5
nelsonic <contact.nelsonic+github@gmail.com>
2016-06-20 17:56:41 +0100

    scroll further down the page. see: https://isearch-ui.s3-eu-west-1.amazonaws.com/reports/screenshots/1.0.0/index.html

--- test/e2e/tc.js
+++ test/e2e/tc.js
@@ -1,5 +1,9 @@
 module.exports = {
   'Inspirational Search' : function (browser) {
+    var height = browser.window.innerHeight
+    console.log('height:', height);
+    // || document.documentElement.clientHeight
+    // || document.body.clientHeight;
     browser
       .url('http://www.tcdl.io.s3-website-eu-west-1.amazonaws.com/isearch/0.18/index.html')
       .waitForElementVisible('body', 1000)
@@ -8,11 +12,11 @@ module.exports = {
       .waitForElementVisible('.list-group-item', 3000)
       .saveScreenshot(GLOBAL.IMGPATH() + 'tc_autocomplete.png')
       .click('.list-group-item')
-      .pause(3000)
+      .pause(5000)
       .saveScreenshot(GLOBAL.IMGPATH() + 'tc_result.png')
       // scroll .articleTileImage
-      .moveToElement('.articleTileImage', 0, 400)
-      .pause(200)
+      .moveToElement('.articleTileImage', 200, 800)
+      .pause(500)
       .saveScreenshot(GLOBAL.IMGPATH() + 'tc_result_article.png')
       .end();
   }
