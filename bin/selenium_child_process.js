c250b444b41ec4fc1f04c818d4c537dce1d9836e
nelsonic <contact.nelsonic@gmail.com>
2016-06-26 13:27:16 +0100

    only download selenium & chromedriver dont run them yet

--- bin/selenium_child_process.js
+++ bin/selenium_child_process.js
@@ -6,25 +6,26 @@ selenium.ensure('./bin', function(error) {
   if (error) {
     throw new Error(error);
   } else {
-    return start();
+    // return start();
+    console.log('Selenium & Chromedriver downloaded.')
   }
 });
 
-function start () {
-  get('http://localhost:4444/wd/hub/status', function (res) {
-    if (res && res.statusCode === 200){
-      res.on("data", function(chunk) {
-        console.log('Selenium is running:',
-          JSON.stringify(JSON.parse(chunk), null, 2));
-        return; // res.end is automatically called.
-      });
-    }
-  }).on('error', (e) => {
-    console.log(`Selenium NOT Running (yet) ... ${e.message}`);
-    console.log('Starting Selenium ...');
-    var cmd = 'nohup java -jar ./bin/selenium.jar &';
-    console.log('$ ' + cmd); // give user some feedback
-    require('daemon')(); // run the selenium process as a daemon (background)
-    execSync(cmd);
-  });
-}
+// function start () {
+//   get('http://localhost:4444/wd/hub/status', function (res) {
+//     if (res && res.statusCode === 200){
+//       res.on("data", function(chunk) {
+//         console.log('Selenium is running:',
+//           JSON.stringify(JSON.parse(chunk), null, 2));
+//         return; // res.end is automatically called.
+//       });
+//     }
+//   }).on('error', (e) => {
+//     console.log(`Selenium NOT Running (yet) ... ${e.message}`);
+//     console.log('Starting Selenium ...');
+//     var cmd = 'nohup java -jar ./bin/selenium.jar &';
+//     console.log('$ ' + cmd); // give user some feedback
+//     require('daemon')(); // run the selenium process as a daemon (background)
+//     execSync(cmd);
+//   });
+// }
