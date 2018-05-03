eae32de576783f14f541abdb5af2ea0874360c80
Siim Tiilen <eritikass@gmail.com>
2018-05-03 16:59:12 +0300

    add all

--- test/e2e/google.js
+++ test/e2e/google.js
@@ -3,13 +3,22 @@ var config = require('../../nightwatch.conf.js');
 module.exports = { // addapted from: https://git.io/vodU0
   '@tags': ['google'],
   'Open google': function(browser) {
+    // load google
     browser
       .url('https://www.google.ee/')
       .pause(1000)
       .waitForElementVisible('body')
       .pause(1000)
-      .saveScreenshot(config.imgpath(browser) + 'google1.png')
-      .end();
+      .saveScreenshot(config.imgpath(browser) + 'google1.png');
+    
+    // search kassid
+    browser
+        .setValue('#lst-ib', ['cat', browser.Keys.ENTER])
+        .saveScreenshot(config.imgpath(browser) + 'google2.png')
+        .pause(10000)
+        .saveScreenshot(config.imgpath(browser) + 'google3.png')
+        .assert.containsText('body', 'kass')
+        .end();
 
   }
 }
