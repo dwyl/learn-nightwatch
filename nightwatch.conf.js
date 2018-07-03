95ed3ffe3f1d6318ceff4bd17c8fbacee999a94e
raichel <eran.raichel@gmail.com>
2018-07-03 22:25:47 +0300

    Handle tests with dockers

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -14,7 +14,7 @@ const config = { // we use a nightwatch.conf.js file so we can include comments 
     "server_path": seleniumServer.path,
     "log_path": "",
     "host": "hub",
-    "port": 31234,
+    "port": 4444,
     "cli_args": {
       "webdriver.chrome.driver" : chromedriver.path
     }
@@ -38,7 +38,7 @@ const config = { // we use a nightwatch.conf.js file so we can include comments 
     },
     "local": {
       "launch_url": "http://localhost",
-      "selenium_port": 31234,
+      "selenium_port": 4444,
       "selenium_host": "chrome",
       "silent": true,
       "screenshots": {
